985ec02b6d4c566504e720a4916d62ea151c4e8c
uni <cbcb9995@naver.com>
2026-06-01 18:00:59 +0900

    [PG] 정윤희 구명보트

--- week05/yunhee/Solution_42885.java
+++ week05/yunhee/Solution_42885.java
@@ -10,25 +10,22 @@ import java.util.Arrays;
 
 class Solution_42885 {
     public int solution(int[] people, int limit) {
-
+    	
         Arrays.sort(people);
 
         int left = 0;
         int right = people.length - 1;
         int boat = 0;
-
+        
         while (left <= right) {
-
             // 가장 가벼운 사람이 가장 무거운 사람이 함께 탈 수 있으면 
             if (people[left] + people[right] <= limit) {
                 left++;
             }
-
             // 가장 무거운 사람은 항상 태움 
             right--;
             boat++;
         }
-
         return boat;
     }
 }
